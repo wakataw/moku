fdcce63f4ee5e83acf38049fb2906fd11a559056
wakataw <agungpratama1001@gmail.com>
2022-09-06 15:41:29 +0700

    update enrollment entity

--- entity/enrollment.go
+++ entity/enrollment.go
@@ -2,6 +2,8 @@ package entity
 
 type Enrollment struct {
 	ID        int `gorm:"primaryKey;autoIncrement" json:"id"`
+	UserID    int `json:"user_id"`
+	User      User
 	ProgramID int `json:"program_id"`
 	Program   Program
 }
